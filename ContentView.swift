96b7c4482fd6cdbd4a6abc32be6615bd92918e51
oopsuwu <60373320+BeeLeDev@users.noreply.github.com>
2024-08-02 14:39:35 -0400

    feat: display list of desserts

--- ContentView.swift
+++ ContentView.swift
@@ -9,18 +9,63 @@
  https://fetch-hiring.s3.amazonaws.com/iOS+coding+exercise.pdf
  */
 
+//
+
 import SwiftUI
 
 struct ContentView: View {
+    @State private var meals = [Meal]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text("Desserts")
+            .font(.title)
+            .bold()
+        
+        // sort list in alphabetical order
+        List(meals.sorted(by: { $0.strMeal < $1.strMeal }), id: \.idMeal) { meal in
+            VStack {
+                Text(meal.strMeal)
+            }
         }
-        .padding()
+        .scrollContentBackground(.visible)
+        .task {
+            await loadData()
+        }
+        
+        // ...
     }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert") 
+        else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            // we only want the data, not metadata
+            let (data, _) = try await URLSession.shared.data(from: url)
+
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                meals = decodedResponse.meals
+            }
+            
+        } catch {
+        print("Invalid data")
+        }
+    }
+    
+    // ...
+}
+
+struct Response: Codable {
+    var meals: [Meal]
+}
+
+struct Meal: Codable {
+    var strMeal: String
+    var strMealThumb: String
+    var idMeal: String
 }
 
 #Preview {
