3629fa6fb21a9cf5be66f51557fec2bc7828fc6c
oopsuwu <60373320+BeeLeDev@users.noreply.github.com>
2024-08-02 12:44:00 -0400

    add XCode files

---
+++ ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  FetchCodingChallenge
+//
+//  Created by BLeDev on 8/1/24.
+//
+
+/*
+ https://fetch-hiring.s3.amazonaws.com/iOS+coding+exercise.pdf
+ */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FetchCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchCodingChallengeApp.swift
+//  FetchCodingChallenge
+//
+//  Created by BLeDev on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
