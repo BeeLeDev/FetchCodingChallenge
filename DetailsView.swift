93f25e511addae5f8b8a5e7068e4977e6e98d9f1
oopsuwu <60373320+BeeLeDev@users.noreply.github.com>
2024-08-05 14:48:05 -0400

    refactor: improved JSON decoding with CodingKeys

--- DetailsView.swift
+++ DetailsView.swift
@@ -8,17 +8,16 @@
 import SwiftUI
 
 struct DetailsView: View {
-    @State private var mealDetails = MealDetails(idMeal: "", strMeal: "", strMealThumb: "", strInstructions: "")
+    // this is needed since the structure of the JSON is represented as an array with one element
+    @State private var mealDetails = MealDetails()
     var id: String
-    
-    @State private var ingredients: [String] = []
-    @State private var measurements: [String] = []
         
     var body: some View {
-        // TODO: lookup the dessert by id as it will give more info
         ScrollView {
             AsyncImage(url: URL(string: mealDetails.strMealThumb)) { image in
-                image.image?.resizable()
+                image.resizable()
+            } placeholder: {
+                ProgressView()
             }
             .frame(width: 350, height: 300)
             .clipShape(Circle())
@@ -27,175 +26,111 @@ struct DetailsView: View {
             }
             .shadow(radius: 7)
             
+            
             VStack(alignment: .leading) {
                 Text(mealDetails.strMeal)
                     .font(.title)
                     .bold()
-
+                    .frame(maxWidth: .infinity, alignment: .center)
+                
                 Divider()
                 
                 Text("Instructions")
-                .font(.title2)
-                .bold()
+                    .font(.title2)
+                    .bold()
                 Text(mealDetails.strInstructions)
                 
                 Divider()
                 
                 Text("Ingredients/Measurements")
-                .font(.title2)
-                .bold()
+                    .font(.title2)
+                    .bold()
                 
-                ForEach(ingredients, id: \.self) { ingredient in
+                ForEach(mealDetails.ingredients, id: \.self) { ingredient in
                     if !(ingredient == "") {
-                        if let index = ingredients.firstIndex(of: ingredient) {
-                            let text = measurements.count > 0 && measurements.indices.contains(index) ? measurements[index] : "Measurement not found"
-                            Text("\(ingredient): \(text)")
+                        if let index = mealDetails.ingredients.firstIndex(of: ingredient) {
+                            let measurement = mealDetails.measurements[index]
+                            Text("\(ingredient): \(measurement)")
                         }
                     }
                 }
-                
-                    
-                
             }
             .padding()
-            
         }
         .task {
             await loadMealDetails(id: id)
         }
         
-        
     }
     
     func loadMealDetails(id: String) async {
-        
-        print("Loading ID: \(id)")
-
-        
         guard let url = URL(string: "https://www.themealdb.com/api/json/v1/1/lookup.php?i=\(id)")
         else {
             print("Invalid URL")
             return
         }
         
-        
         do {
-            // we only want the data, not metadata
             let (data, _) = try await URLSession.shared.data(from: url)
-
             if let decodedResponse = try? JSONDecoder().decode(MealDetailsResponse.self, from: data) {
                 for meal in decodedResponse.meals {
-                    mealDetails.idMeal = meal.idMeal
-                    mealDetails.strMeal = meal.strMeal
-                    mealDetails.strMealThumb = meal.strMealThumb
-                    mealDetails.strInstructions = meal.strInstructions
-                    
-                    ingredients.append(meal.strIngredient1 ?? "")
-                    ingredients.append(meal.strIngredient2 ?? "")
-                    ingredients.append(meal.strIngredient3 ?? "")
-                    ingredients.append(meal.strIngredient4 ?? "")
-                    ingredients.append(meal.strIngredient5 ?? "")
-                    ingredients.append(meal.strIngredient6 ?? "")
-                    ingredients.append(meal.strIngredient7 ?? "")
-                    ingredients.append(meal.strIngredient8 ?? "")
-                    ingredients.append(meal.strIngredient9 ?? "")
-                    ingredients.append(meal.strIngredient10 ?? "")
-                    ingredients.append(meal.strIngredient11 ?? "")
-                    ingredients.append(meal.strIngredient12 ?? "")
-                    ingredients.append(meal.strIngredient13 ?? "")
-                    ingredients.append(meal.strIngredient14 ?? "")
-                    ingredients.append(meal.strIngredient15 ?? "")
-                    ingredients.append(meal.strIngredient16 ?? "")
-                    ingredients.append(meal.strIngredient17 ?? "")
-                    ingredients.append(meal.strIngredient18 ?? "")
-                    ingredients.append(meal.strIngredient19 ?? "")
-                    ingredients.append(meal.strIngredient20 ?? "")
-                    
-                    measurements.append(meal.strMeasure1 ?? "")
-                    measurements.append(meal.strMeasure2 ?? "")
-                    measurements.append(meal.strMeasure3 ?? "")
-                    measurements.append(meal.strMeasure4 ?? "")
-                    measurements.append(meal.strMeasure5 ?? "")
-                    measurements.append(meal.strMeasure6 ?? "")
-                    measurements.append(meal.strMeasure7 ?? "")
-                    measurements.append(meal.strMeasure8 ?? "")
-                    measurements.append(meal.strMeasure9 ?? "")
-                    measurements.append(meal.strMeasure10 ?? "")
-                    measurements.append(meal.strMeasure11 ?? "")
-                    measurements.append(meal.strMeasure12 ?? "")
-                    measurements.append(meal.strMeasure13 ?? "")
-                    measurements.append(meal.strMeasure14 ?? "")
-                    measurements.append(meal.strMeasure15 ?? "")
-                    measurements.append(meal.strMeasure16 ?? "")
-                    measurements.append(meal.strMeasure17 ?? "")
-                    measurements.append(meal.strMeasure18 ?? "")
-                    measurements.append(meal.strMeasure19 ?? "")
-                    measurements.append(meal.strMeasure20 ?? "")
-                    
-                    print(ingredients)
-                    print(measurements)
-                    print("Successfully loaded ID: \(id)")
+                    mealDetails = meal
                 }
             }
+            
         } catch {
         print("Invalid data")
         }
     }
 }
 
-struct MealDetailsResponse: Codable {
+// struct of the JSON data
+struct MealDetailsResponse: Decodable {
     var meals: [MealDetails]
 }
 
-struct MealDetails: Codable {
-    var idMeal: String
+struct MealDetails: Decodable, Identifiable {
+    var id: String
     var strMeal: String
     var strMealThumb: String
     var strInstructions: String
+    var ingredients: [String]
+    var measurements: [String]
     
-    var strIngredient1: String?
-    var strIngredient2: String?
-    var strIngredient3: String?
-    var strIngredient4: String?
-    var strIngredient5: String?
-    var strIngredient6: String?
-    var strIngredient7: String?
-    var strIngredient8: String?
-    var strIngredient9: String?
-    var strIngredient10: String?
-    var strIngredient11: String?
-    var strIngredient12: String?
-    var strIngredient13: String?
-    var strIngredient14: String?
-    var strIngredient15: String?
-    var strIngredient16: String?
-    var strIngredient17: String?
-    var strIngredient18: String?
-    var strIngredient19: String?
-    var strIngredient20: String?
+    // https://stackoverflow.com/questions/68213705/swift-initialise-model-object-with-initfrom-decoder
+    private enum CodingKeys: String, CodingKey {
+        case idMeal
+        case strMeal
+        case strMealThumb
+        case strInstructions
+        case strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12, strIngredient13, strIngredient14, strIngredient15, strIngredient16, strIngredient17, strIngredient18, strIngredient19, strIngredient20
+        case strMeasure1, strMeasure2, strMeasure3, strMeasure4, strMeasure5, strMeasure6, strMeasure7, strMeasure8, strMeasure9, strMeasure10, strMeasure11, strMeasure12, strMeasure13, strMeasure14, strMeasure15, strMeasure16, strMeasure17, strMeasure18, strMeasure19, strMeasure20
+    }
     
-    var strMeasure1: String?
-    var strMeasure2: String?
-    var strMeasure3: String?
-    var strMeasure4: String?
-    var strMeasure5: String?
-    var strMeasure6: String?
-    var strMeasure7: String?
-    var strMeasure8: String?
-    var strMeasure9: String?
-    var strMeasure10: String?
-    var strMeasure11: String?
-    var strMeasure12: String?
-    var strMeasure13: String?
-    var strMeasure14: String?
-    var strMeasure15: String?
-    var strMeasure16: String?
-    var strMeasure17: String?
-    var strMeasure18: String?
-    var strMeasure19: String?
-    var strMeasure20: String?
-
-   
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try values.decode(String.self, forKey: .idMeal)
+        self.strMeal = try values.decode(String.self, forKey: .strMeal)
+        self.strMealThumb = try values.decode(String.self, forKey: .strMealThumb)
+        self.strInstructions = try values.decode(String.self, forKey: .strInstructions)
+        // compact maps remove null values
+        self.ingredients = (1...20).compactMap { index in
+            try? values.decode(String.self, forKey: CodingKeys(rawValue: "strIngredient\(index)")!)
+        }
+        self.measurements = (1...20).compactMap { index in
+            try? values.decode(String.self, forKey: CodingKeys(rawValue: "strMeasure\(index)")!)
+        }
+    }
+    
+    // created to be able to intialize variable mealDetails without the from decoder param
+    init() {
+        id = ""
+        strMeal = ""
+        strMealThumb = ""
+        strInstructions = ""
+        ingredients = []
+        measurements = []
+    }
 }
 
 #Preview {
